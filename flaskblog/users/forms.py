a364be43750446459ee22a58853509d237c90ab2
Dhruv Modi <77389175+dhruv01234@users.noreply.github.com>
2022-07-19 00:08:33 +0530

    blogpost

---
+++ flaskblog/users/forms.py
@@ -0,0 +1,60 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from flaskblog.models import User
+
+class ResgistrationForm(FlaskForm):
+    firstname = StringField('First Name',validators=[DataRequired(),Length(min=1,max=20)])
+    lastname = StringField('Last Name',validators=[DataRequired(),Length(min=1,max=10)])
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=10)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username is already taken')
+    
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already registered')
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=10)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    picture = FileField('Update Profile Image',validators=[FileAllowed(['jpg','png','jpeg'])])
+    submit = SubmitField('Update')
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('username is already taken')
+        
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already registered')
+        
+class ReqResForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    submit = SubmitField('Send OTP')
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('Email is not registered')
+    
+class ResPassForm(FlaskForm):
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Reset')
+    
